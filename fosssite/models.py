3f94d0c5f0642b5e0c5a3b2483fb348ee0b96104
sonali0901 <sonaligpt0@gmail.com>
2016-08-19 18:06:53 +0530

    update models.py

--- fosssite/models.py
+++ fosssite/models.py
@@ -1,24 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
-<<<<<<< HEAD
 from django.contrib.auth.models import User
-
-from django.utils import timezone
-from passlib.hash import pbkdf2_sha256
-
-# Create your models here.
-class UserProfile(models.Model):
-	firstname=models.CharField(max_length=200)
-	lastname=models.CharField(max_length=200)
-	username=models.CharField(max_length=200)
-	email=models.ForeignKey('auth.User')
-	password=models.CharField(max_length=200)
-
-	User.profile = property(lambda u: UserProfile.objects.get_or_create(email=u)[0])
-
-	def verify_password(self,raw_password):
-		return pbkdf2_sha256.verify(raw_password,self.password)
-=======
 #from django.utils import timezone
 #from django.core.exceptions import ValidationError
 
@@ -35,4 +17,4 @@ class UserProfile(models.Model):
 
 	def __str__(self):
 		return self.profileuser.username
->>>>>>> 6c068a332a3c30eba70dad6fe128dd8ac985a811
+
